454702a3f232913d12493fde03efb9214982f072
loli hunter <mh1225az@gmail.com>
2024-05-11 22:03:47 +0800

    ini laravel saya

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index', ['nama' => 'fadil']);
 });
+
+Route::get('/home', function () {
+    return view('home', );
+});
+
